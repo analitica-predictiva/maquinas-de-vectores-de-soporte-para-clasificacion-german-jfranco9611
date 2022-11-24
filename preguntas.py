a0bd977ec4171c1cef60f9d9b61156c0a7db5478
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-11-23 21:52:22 -0500

    Pr_08

--- preguntas.py
+++ preguntas.py
@@ -250,11 +250,13 @@ def pregunta_04():
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
     cfm_train = confusion_matrix(
         y_true = y_train,
-        y_pred = pipeline.predict(X_train),   )
+        y_pred = pipeline.predict(X_train),
+    )
 
     cfm_test = confusion_matrix(
         y_true = y_test,
-        y_pred = pipeline.predict(X_test),    )
+        y_pred = pipeline.predict(X_test),
+    )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
     return cfm_train, cfm_test
